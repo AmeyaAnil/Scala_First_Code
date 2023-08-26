95705be69c0da16bb8eae270a4cf08c81c2e94aa
Ameya Anil <ameyaanil141@gmail.com>
2023-08-26 10:36:37 -0700

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+object Main{
+  def main(args: Array[String]){
+    val num1: Int = 13
+    val num2: Int = 16
+    val large = if (num1 > num2) num1 else num2
+    println("Largest number is:" + large)
+
+ }
+
+}
